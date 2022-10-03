14a1727eb11a71c2737c2a28b5a1a68a3bea0263
Mourad HASSINI <mourad.hassini@esprit.tn>
2022-10-03 11:54:01 +0100

    c2nd commit

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -17,15 +17,9 @@ import javax.persistence.TemporalType;
 @Entity
 @Table(name = "T_USER")
 public class User implements Serializable {
-	
-	// commentaitre 3ALINFO5 
-	
-	// commentaire 1 
-	// Commentaire 2 
-	
-	// Commaentaire 4
-	// hhhh
-	// modif 5
+
+	/* 1st comment */
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
